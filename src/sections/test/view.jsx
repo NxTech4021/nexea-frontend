7e73c1b71d25f627beafd1a38135eaf69295a944
afiqnexea <afiqnrzm@hotmail.com>
2024-05-02 02:26:44 +0800

    A

--- src/sections/test/view.jsx
+++ src/sections/test/view.jsx
@@ -1,72 +1,66 @@
-import QrScanner from 'qr-scanner';
-import { useRef, useEffect } from 'react';
-
 const QrReader = () => {
-  // QR States
-  const scanner = useRef(null);
-  const videoEl = useRef(null);
-  const qrBoxEl = useRef(null);
-
-  useEffect(() => {
-    if (videoEl?.current && !scanner.current) {
-      scanner.current = new QrScanner(
-        videoEl?.current,
-        (res) => {
-          console.log('WADWAD', res);
-        },
-        {
-          onDecodeError: (res) => console.log('ERROR'),
-          preferredCamera: 'environment',
-          highlightScanRegion: true,
-          highlightCodeOutline: true,
-          overlay: qrBoxEl?.current || undefined,
-        }
-      );
-
-      scanner?.current.start();
-    }
-    return () => {
-      scanner?.current.stop();
-    };
-  }, []);
-
-  return (
-    <div
-      className="qr-reader"
-      style={{
-        width: '430px',
-        height: '100vh',
-        margin: '0 auto',
-        position: 'relative',
-      }}
-    >
-      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video
-        ref={videoEl}
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-        }}
-      />
-      <div ref={qrBoxEl} className="qr-box">
-        <img
-          src="/test.svg"
-          alt="Qr Frame"
-          width={256}
-          height={256}
-          className="qr-frame"
-          style={{
-            position: 'absolute',
-            fill: 'none',
-            left: '50%',
-            top: '50%',
-            transform: 'translateX(-50%) translateY(-50%)',
-          }}
-        />
-      </div>
-    </div>
-  );
+  // // QR States
+  // const scanner = useRef(null);
+  // const videoEl = useRef(null);
+  // const qrBoxEl = useRef(null);
+  // useEffect(() => {
+  //   if (videoEl?.current && !scanner.current) {
+  //     scanner.current = new QrScanner(
+  //       videoEl?.current,
+  //       (res) => {
+  //         console.log('WADWAD', res);
+  //       },
+  //       {
+  //         onDecodeError: (res) => console.log('ERROR'),
+  //         preferredCamera: 'environment',
+  //         highlightScanRegion: true,
+  //         highlightCodeOutline: true,
+  //         overlay: qrBoxEl?.current || undefined,
+  //       }
+  //     );
+  //     scanner?.current.start();
+  //   }
+  //   return () => {
+  //     scanner?.current.stop();
+  //   };
+  // }, []);
+  // return (
+  //   <div
+  //     className="qr-reader"
+  //     style={{
+  //       width: '430px',
+  //       height: '100vh',
+  //       margin: '0 auto',
+  //       position: 'relative',
+  //     }}
+  //   >
+  //     {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+  //     <video
+  //       ref={videoEl}
+  //       style={{
+  //         width: '100%',
+  //         height: '100%',
+  //         objectFit: 'cover',
+  //       }}
+  //     />
+  //     <div ref={qrBoxEl} className="qr-box">
+  //       <img
+  //         src="/test.svg"
+  //         alt="Qr Frame"
+  //         width={256}
+  //         height={256}
+  //         className="qr-frame"
+  //         style={{
+  //           position: 'absolute',
+  //           fill: 'none',
+  //           left: '50%',
+  //           top: '50%',
+  //           transform: 'translateX(-50%) translateY(-50%)',
+  //         }}
+  //       />
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default QrReader;
