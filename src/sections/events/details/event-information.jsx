9f8534cf5eb21564cc1712cea69a05ae0c1b3aaa
Willy Wong <willywong@Willys-MacBook-Air.local>
2025-04-25 07:38:00 +0800

    Some minor fixes

--- src/sections/events/details/event-information.jsx
+++ src/sections/events/details/event-information.jsx
@@ -1,38 +1,39 @@
 import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import React, { useState, useEffect } from 'react';
-import { Form, Field, Formik, ErrorMessage } from 'formik';
+// import { Form, Field, Formik, ErrorMessage } from 'formik';
 
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import {
   Box,
   Card,
   Chip,
-  Grid,
+  // Grid,
   Stack,
-  Dialog,
-  Avatar,
-  Select,
-  Button,
+  // Dialog,
+  // Avatar,
+  // Select,
+  // Button,
   Divider,
-  MenuItem,
-  TextField,
+  // MenuItem,
+  // TextField,
   keyframes,
   Typography,
-  InputLabel,
+  // InputLabel,
   CardContent,
-  DialogTitle,
-  FormControl,
-  DialogContent,
+  // DialogTitle,
+  // FormControl,
+  // DialogContent,
 } from '@mui/material';
 
-import { endpoints, axiosInstance } from 'src/utils/axios';
+// import { endpoints, axiosInstance } from 'src/utils/axios';
 import { useGetAllEvents } from 'src/api/event';
 
 import Iconify from 'src/components/iconify';
+
 import EditEventModal from '../create/dialog/edit-event-modal';
 
 const EventStatus = {
