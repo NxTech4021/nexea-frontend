58192383bdc54163674160d230df5f5c8f3e11ec
afiq <afiq@nexea.co>
2025-05-21 13:04:16 +0800

    a

--- src/components/markdown/MarkdownContent.jsx
+++ src/components/markdown/MarkdownContent.jsx
@@ -4,37 +4,37 @@ import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
 
-import { styled } from '@mui/material/styles';
+import { styled } from '@mui/material';
 
-// const MarkdownWrapper = styled('div')(({ theme }) => ({
-//   '& p': {
-//     marginBottom: theme.spacing(2),
-//   },
-//   '& a': {
-//     color: theme.palette.primary.main,
-//     textDecoration: 'none',
-//     '&:hover': {
-//       textDecoration: 'underline',
-//     },
-//   },
-//   '& strong': {
-//     fontWeight: 600,
-//   },
-//   '& em': {
-//     fontStyle: 'italic',
-//   },
-//   '& ul, & ol': {
-//     marginBottom: theme.spacing(2),
-//     paddingLeft: theme.spacing(3),
-//   },
-//   '& li': {
-//     marginBottom: theme.spacing(0.5),
-//   },
-//   '& img': {
-//     maxWidth: '100%',
-//     height: 'auto',
-//   },
-// }));
+const MarkdownWrapper = styled('div')(({ theme }) => ({
+  '& p': {
+    marginBottom: theme.spacing(2),
+  },
+  '& a': {
+    color: theme.palette.primary.main,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  '& strong': {
+    fontWeight: 600,
+  },
+  '& em': {
+    fontStyle: 'italic',
+  },
+  '& ul, & ol': {
+    marginBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(3),
+  },
+  '& li': {
+    marginBottom: theme.spacing(0.5),
+  },
+  '& img': {
+    maxWidth: '100%',
+    height: 'auto',
+  },
+}));
 
 const MarkdownContent = ({ content }) => (
   <MarkdownWrapper>
