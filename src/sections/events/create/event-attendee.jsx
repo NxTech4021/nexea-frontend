a28400c72af50ff92c42e6909379a812ed05bc54
afiq <afiq@nexea.co>
2025-08-26 12:08:25 +0800

    a

--- src/sections/events/create/event-attendee.jsx
+++ src/sections/events/create/event-attendee.jsx
@@ -749,7 +749,7 @@ export default function EventAttendee() {
         // Check if there's an add-on
         const { ticketAddOn } = row.ticket;
 
-        if (!ticketAddOn || !ticketAddOn?.addOn) {
+        if (!ticketAddOn && !ticketAddOn?.addOn) {
           return 'none';
         }
 
